b19d42e2ef14b17b8038a7aa691630deca50d10d
Gloria Guy <totallygloria@gmail.com>
2017-03-07 09:03:42 -0800

    Gets stateful class component to load successfully
    Had a typo where I was importing incorrectly so component wasn't
    loading.
    
    import { React, Component } from 'react';  // doesn't work
    
    import React, { Component } from 'react';  // correct import
    
    Taking React out of the destructuring fixed the bug.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import './index.css';
 
+
+export default class ImageGallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'list'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>there's a component</p>
+      </div>
+    )
+  }
+}
+
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+    <div>
+      <h1>Image Gallery</h1>
+      <ImageGallery />  
+    </div>,
+    document.getElementById('root')
+)
