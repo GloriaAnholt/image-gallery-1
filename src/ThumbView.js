b7d93758243a76262d6d0ceea4544723e52c43a4
Gloria Guy <totallygloria@gmail.com>
2017-03-07 12:45:32 -0800

    Refactors thumb view to render an array of thumbnails. Works

--- src/ThumbView.js
+++ src/ThumbView.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 export default function ThumbView(props) {
 // "thumbnail" - the image as a "thumbnail" (within a 100x100 pixel scale) and the title . 
+  const imagesHtml = props.images.map((image, i) => {
+      let key = i + image.title;
+      return (
+      <div key={ key }>
+        <img src={ image.url } alt={ image.title } height="100" width="100" />
+        <p>{ image.title }</p>
+      </div>
+  )
+  })
   return (
       <div>
         <h2>Thumb View</h2>
-        <img src={props.images.url} alt={props.images.title} height="100" width="100" />
-        <p>{props.images.title}</p>
+        { imagesHtml }
       </div>
   )
 }
