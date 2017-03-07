7c2d67f4f91070423c65629945e68ec31fc251c9
Gloria Guy <totallygloria@gmail.com>
2017-03-07 12:13:24 -0800

    Moves image html creation into a map to render all images on props
    Instead of rendering just a single image, the GalleryView now takes
    in an array of images and maps over them, generating the img tag
    and html for each image in the array, returning the completed list
    of images (with unique 'keys' for the divs containing the images).
    
    Correctly displays all 4 images.

--- src/GalleryView.js
+++ src/GalleryView.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 export default function GalleryView(props) {
 // "gallery" - Display the title, the full-size image, and the description.
+  const imagesHtml = props.images.map((image, i) => {
+      let key = i + image.title;
+      return (
+        <div key={ key }>
+            <img src={ image.url } alt={ image.title } />
+            <h3>{ image.title }</h3>
+            <p>{ image.description }</p>
+        </div>
+      )
+  })
+  
   return (
       <div>
         <h2>Gallery View</h2>
-        <img src={props.images.url} title={props.images.title} />
-        <h3>{props.images.title}</h3>
-        <p>{props.images.description}</p>
+        { imagesHtml }
       </div>
   )
 }
