762a7508816c685bf0a2fa65c54c34980bdc8df7
Gloria Guy <totallygloria@gmail.com>
2017-03-07 13:06:13 -0800

    Updates listview component to render each image as text description
    The list view maps over the array of images passed in on the prop
    object and makes the html for rendering the images as text.
    
    Displays as expected.

--- src/ListView.js
+++ src/ListView.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 export default function ListView(props) {
 // "list" - display the title of the image, the link (not the  actual image), and description.  
+  const imagesHtml = props.images.map((image, i) => {
+      let key = i + image.title;
+      return (
+          <div key={ key }>
+              <h3>{ image.title }</h3>
+              <p>{ image.description }</p>
+              <p>{ image.url }</p>
+          </div>
+      )
+  })
+  
   return (
-    <h2>List View</h2>
+      <div>
+        <h2>List View</h2>
+        { imagesHtml }
+      </div>
   )
 }
