391f8e7edb781d98fe48ab323e5d1a5e94902a9c
Gloria Guy <totallygloria@gmail.com>
2017-03-07 09:11:50 -0800

    Adds structure for stateless functional components for view types.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom'
 import './index.css';
 
 
+function ListView(props) {
+  // "list" - display the title of the image, the link (not the  actual image), and description.  
+  return (
+    <h2>List View</h2>
+  )
+}
+
+function ThumbView(props) {
+  // "thumbnail" - the image as a "thumbnail" (within a 100x100 pixel scale) and the title . 
+  return (
+    <h2>Thumb View</h2>
+  )
+}
+
+function GalleryView(props) {
+  // "gallery" - Display the title, the full-size image, and the description. 
+  return (
+    <h2>Gallery View</h2>
+  )
+}
+
 export default class ImageGallery extends Component {
   constructor(props) {
     super(props);
