9e19310ac548cdf953458f095b141200ec0038c2
Gloria Guy <totallygloria@gmail.com>
2017-03-07 22:55:40 -0800

    Adds a passing snapshot test for the app level component.

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,28 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import Gallery from '../src/GalleryView';
-import List from '../src/ListView';
-import Thumbnail from '../src/ThumbView';
-import App from '../src/index';
+import ImageGallery from '../src/App';
 
-const images = [
-  {
-    title: 'Rainbow frog',
-    description: 'I\'m probably poisonious.',
-    url: 'https://s-media-cache-ak0.pinimg.com/736x/bf/4e/40/bf4e4067252227bd3f758bba7dcee2ff.jpg'
-  },
-  {
-    title: 'Green leaf frogs',
-    description: 'We\'re so cute!',
-    url: 'http://www.savethefrogs.com/amphibians/images/south-america/Hypsiboas-cinerascens-8a.jpg'
-  },
-]
 
 describe('The Image Gallery React App', () => {
 
     it('Renders ths app', () => {
-        const rendered = shallow(App);
+        const rendered = shallow(<ImageGallery />);
         expect(toJson(rendered)).toMatchSnapshot();
     });
 
