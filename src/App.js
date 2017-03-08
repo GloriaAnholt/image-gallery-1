166069d6b6eac0e8893372d4d9cc8069d17dfc0c
Gloria Guy <totallygloria@gmail.com>
2017-03-07 22:51:53 -0800

    Removes render from App, index is only render - to pass jest tests
    Jest snapshot tests were failing because the render method was being
    called in the same module as the app, but the tests don't have access
    to a DOM to mount components into.
    
    This copies the old index into App and removes just the render function,
    and then removes all of the other scripts from the index.js
    
    Works.

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import './index.css';
+import ListView from './ListView';
+import ThumbView from './ThumbView';
+import GalleryView from './GalleryView';
+import images from './images.json';
+
+
+function SelectorButtons(props) {
+  return (
+    <div>
+      <p>Select a view type</p>
+      <button onClick={ () => props.setView('list') }>List view</button>
+      <button onClick={ () => props.setView('thumb') }>Thumbnail view</button>
+      <button onClick={ () => props.setView('gallery') }>Gallery view</button>
+    </div>
+  )
+}
+
+function SelectView(props) {
+  let components = { 'list': ListView, 'thumb': ThumbView, 'gallery': GalleryView }
+  let Selected = components[props.view];
+  return ( <Selected images={images} /> )
+}
+
+SelectView.propTypes = {
+  images: React.PropTypes.array
+}
+
+export default class ImageGallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'list'
+    }
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(selection) {
+    this.setState({ view: selection })
+  }
+
+  render() {
+    return (
+      <div>
+        <SelectorButtons setView={ this.setView } />
+        <SelectView view={ this.state.view } /> 
+      </div>
+    )
+  }
+}
