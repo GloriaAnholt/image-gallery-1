f3457703eef7dfce95569d6752a3c069e7080783
Gloria Guy <totallygloria@gmail.com>
2017-03-08 15:56:27 -0800

    Re-adds the map function to the Detail component
    Something in a renaming accidentally reverted the ListView back to
    an older version (using stash, no doubt), so it lost the ability to
    map over an array of images. This replaces the map so it works again.

--- src/DetailView.js
+++ src/DetailView.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 export default function ListView(props) {
 // "list" - display the title of the image, the link (not the  actual image), and description.  
-    return (
-        <div>
-            <h3>{ props.image.title }</h3>
-            <p>{ props.image.description }</p>
-            <p>{ props.image.url }</p>
+    const imagesHtml = props.images.map((image, i) => {
+      let key = i + image.title;
+      return (
+        <div key={ key }>
+            <h3>{ image.title }</h3>
+            <p>{ image.description }</p>
+            <p>{ image.url }</p>
         </div>
     )
+  })
+  return (
+    <div>
+        <h2>Detailed View</h2>
+        { imagesHtml }
+      </div>
+  )
 }
+
